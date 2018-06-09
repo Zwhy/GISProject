ae1137c3fcd89f092ee2846c9485bfb820aceddd
Zwhy <401002584@qq.com>
2018-06-09 18:53:42 +0800

    关于点的最基本迷你GIS

---
+++ Project_1/BasicClasses.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace MyGIS
+{
+    /*
+     * 节点
+     */
+     class GISVertex
+    {
+        public double x;
+        public double y;
+
+        public GISVertex(double _x, double _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public double Distance(GISVertex anotherVertex)
+        {
+            return System.Math.Sqrt((x - anotherVertex.x) * (x - anotherVertex.x)
+                + (y - anotherVertex.y) * (y - anotherVertex.y));
+        }
+    }
+    /*
+     * 实体点
+     */
+     class GISPoint
+    {
+        public GISVertex Location;
+        public string Attribute;
+
+        public GISPoint(GISVertex oneVertex, string oneString)
+        {
+            Location = oneVertex;
+            Attribute = oneString;
+        }
+        public void DrawPoint(Graphics graphics)
+        {
+            graphics.FillEllipse(new SolidBrush(Color.Red),
+                new Rectangle((int)(Location.x) - 3, (int)(Location.y) - 3, 6, 6));
+        }
+        public void DrawAttribute(Graphics graphics)
+        {
+            graphics.DrawString(Attribute, new Font("宋体", 20), new SolidBrush(Color.Green),
+                                new PointF((int)(Location.x), (int)(Location.y)));
+        }
+        public double Distance(GISVertex anotherVertex)
+        {
+            return Location.Distance(anotherVertex);
+        }
+    }
+    /*
+     * 线实体
+     */
+    class GISLine
+    {
+        List<GISVertex> AllVertexs;
+    }
+    /**
+     * 面实体
+     */
+    class GISPolygon
+    {
+        List<GISVertex> AllVertexs;
+    }
+}
--- Project_1/Form1.cs
+++ Project_1/Form1.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyGIS;
 
 namespace Project_1
 {
     public partial class Form1 : Form
     {
+        List<GISPoint> points = new List<GISPoint>();
         public Form1()
-        {
+        {            
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            string attribute = textBox3.Text;
+            GISVertex oneVertex = new GISVertex(x, y);
+            GISPoint onePoint = new GISPoint(oneVertex, attribute);
+            Graphics graphics = this.CreateGraphics();
+            onePoint.DrawPoint(graphics);
+            onePoint.DrawAttribute(graphics);
+            points.Add(onePoint);
+        }
+        /*
+         * 鼠标点击出现点属性
+         */
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            GISVertex oneVertex = new GISVertex((double)e.X, (double)e.Y);
+            double minDistance = Double.MaxValue;
+            int findId = -1;
+            for (int i = 0; i < points.Count; i++)
+            {
+                double distance = points[i].Distance(oneVertex);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    findId = i;
+                }
+            }
+            if (minDistance > 5 || findId == -1)
+            {
+                MessageBox.Show("没有实体点或者鼠标点击位置不准确！");
+            }
+            else
+            {
+                MessageBox.Show(points[findId].Attribute);
+            }
         }
     }
 }
